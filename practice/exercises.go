b10f1cbe843c61ebd4372aae8a2731cf354022b0
unsubstantiated-Script <jusfulton@gmail.com>
2023-11-24 13:19:29 -0800

    foundations and practice

---
+++ practice/exercises.go
@@ -0,0 +1,110 @@
+package practice
+
+func Exercises() {
+	//color := "green"
+	//
+	//color = "dark " + color
+	//
+	//fmt.Println(color)
+
+	//n := 0.
+	//
+	//n = 3.14 * float64(2)
+	//fmt.Println(n)
+
+	//var (
+	//	perimeter     int
+	//	width, height = 5, 6
+	//)
+	//
+	//perimeter = width * height
+	//
+	//fmt.Println(perimeter)
+
+	//var (
+	//	lang    string
+	//	version int
+	//)
+	//
+	//lang, version = "lang", 2
+	//
+	//fmt.Println(lang, version)
+
+	//var (
+	//	planet string
+	//	isTrue bool
+	//	temp   float64
+	//)
+	//
+	//planet, isTrue, temp = "Mars", true, 19.5
+	//
+	//fmt.Printf("Air is good on %s \n", planet)
+	//fmt.Printf("It's %t \n", isTrue)
+	//fmt.Printf("It is %f degrees \n", temp)
+	//var (
+	//	a int
+	//	b int
+	//)
+	//
+	////_ ignores one of the returns
+	//_, b = multi()
+	//
+	//fmt.Println(a, b)
+
+	//color, color2 := "red", "blue"
+	//
+	//color, color2 = "orange", "green"
+	//
+	//fmt.Println(color, color2)
+	//
+	//red, blue := "red", "blue"
+	//red, blue = "blue", "red"
+	//
+	//fmt.Println(red, blue)
+	//
+	//route, _ := path.Split("secret/file.txt")
+	//
+	//fmt.Println(route)
+
+	//a, b := 10, 5.5
+	//fmt.Println(float64(a) + b)
+
+	//a, b := 10, 5.5
+	//a = int(b)
+	//fmt.Println(a)
+	//fmt.Println(float64(a) + b)
+
+	//fmt.Println(5.5)
+
+	//age := 2
+	//fmt.Println(7.5 + float64(age))
+
+	// DO NOT TOUCH THESE VARIABLES
+	//min := int8(127)
+	//max := int16(1000)
+	//
+	//// FIX THE CODE HERE
+	//fmt.Println(max + int16(min))
+	//
+	//	//UNCOMMENT & FIX THIS CODE
+	//	count := len(os.Args) - 1
+	//
+	//	//UNCOMMENT IT & THEN DO NOT TOUCH THIS CODE
+	//	fmt.Printf("There are %d names.\n", count)
+	//}
+
+	//func multi() (int, int) {
+	//	return 5, 4
+
+	//fmt.Printf("My path var is %s", os.Args[0])
+
+	//fmt.Printf("Hey %s \n", os.Args[1])
+	//fmt.Println("How goes it?")
+
+	//fmt.Printf("There are %d people \n", len(os.Args)-1)
+	//fmt.Printf("Hello %s  \n", os.Args[1])
+	//fmt.Printf("Hello %s  \n", os.Args[2])
+	//fmt.Printf("Hello %s  \n", os.Args[3])
+	//fmt.Println("How goes it?")
+
+}
