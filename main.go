8bee634206b24a5dd9e4c37231bfbf43c016c88a
unsubstantiated-Script <jusfulton@gmail.com>
2023-11-25 17:03:42 -0800

    Structs dones

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
+import (
+	"go1K/practice/json_read_write"
+)
+
 func main() {
 	//	practice.IntroStuffs()
 	//practice.Exercises()
 	//practice.StringStuff()
 	//practice.StructsStuff()
 	//practice.LogParser()
+	//practice.JSONStructWrite()
+	json_read_write.JSONStructRead()
 	//practice.FunctionStuff()
 
 }
