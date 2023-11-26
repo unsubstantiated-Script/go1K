6c1c5d4d086ec7ce131808205bd3c8b3de4fadc2
unsubstantiated-Script <jusfulton@gmail.com>
2023-11-25 17:06:24 -0800

    Some refactorz

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"go1K/practice/json_read_write"
-)
+import "go1K/practice/json_read_write"
 
 func main() {
 	//	practice.IntroStuffs()
@@ -10,7 +8,7 @@ func main() {
 	//practice.StringStuff()
 	//practice.StructsStuff()
 	//practice.LogParser()
-	//practice.JSONStructWrite()
+	//json_read_write.JSONStructWrite()
 	json_read_write.JSONStructRead()
 	//practice.FunctionStuff()
 
